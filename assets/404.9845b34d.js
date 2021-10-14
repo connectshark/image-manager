6d181a63c792455208c63bfa90c1a41792f6bca3
connectshark <46649021+connectshark@users.noreply.github.com>
2021-10-14 09:14:57 +0000

    Deploying to gh-pages from @ connectshark/image-manager@9d53c0f5969371ee91f758021998b9eebe9b2354 🚀

--- assets/404.9845b34d.js
+++
@@ -1,1 +0,0 @@
-import{c as e,o as r}from"./vendor.99ef0a7f.js";const n={setup:()=>({})};n.render=function(n,o,t,s,a,f){return r(),e("h2",null,"404")};export{n as default};
---
+++ assets/404.cda8f6d1.js
@@ -0,0 +1,1 @@
+import{r as a,c as o,e as s,w as e,F as r,b as t,o as n,f as l}from"./vendor.99ef0a7f.js";const f={},u=t("h2",null,"404",-1),c=l("back to school");f.render=function(t,l){const f=a("router-link");return n(),o(r,null,[u,s(f,{to:"/view"},{default:e((()=>[c])),_:1})],64)};export{f as default};
